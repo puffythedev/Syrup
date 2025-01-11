cd06b23ec71bfd0b43b842fe57d4247a8806fed2
Amine <NicknameOPG1@gmail.com>
2025-01-11 19:11:47 +0100

    a

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ void collect(){
 
 void replaceAll(std::string &str, const std::string &from, const std::string &to) {
     size_t startPos = 0;
-    while ((startPos = str.find(from, startPos)) != string::npos) {
+    while ((startPos = str.find(from, startPos)) != std::string::npos) {
         str.replace(startPos, from.length(), to);
         startPos += to.length();
     }
